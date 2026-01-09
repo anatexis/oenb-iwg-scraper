c2b034963662a6eb037b2b68539112f928f0f6e3
anatexis <anatexis@users.noreply.github.com>
2026-01-09 20:06:37 +0100

    feat: define DownloadItem schema

--- scraper/oenb_scraper/items.py
+++ scraper/oenb_scraper/items.py
@@ -1,12 +1,17 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class OenbScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class DownloadItem(scrapy.Item):
+    """Represents a downloadable resource found on oenb.at"""
+    url = scrapy.Field()
+    type = scrapy.Field()  # 'download', 'shiny_app', 'external_data'
+    file_type = scrapy.Field()  # 'pdf', 'xlsx', 'csv', 'xml', 'zip', etc.
+    file_size_bytes = scrapy.Field()
+    title = scrapy.Field()
+    found_on_page = scrapy.Field()
+    page_section = scrapy.Field()
+    section_heading = scrapy.Field()
+    page_date = scrapy.Field()
+    scraped_at = scrapy.Field()
+    machine_readable = scrapy.Field()  # For PDFs: True/False/None
+    has_tables = scrapy.Field()  # For PDFs: True/False/None
